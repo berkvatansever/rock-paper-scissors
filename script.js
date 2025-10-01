3d1252e1e60ebc8b287633ace53aeb78614f055a
BerkVato <93997736+BerkVato@users.noreply.github.com>
2025-10-01 18:34:41 +0300

    add game logic

--- script.js
+++ script.js
@@ -2,22 +2,60 @@ function getComputerChoice() {
      let random = Math.floor(Math.random() * 3);
       
     if (random == 0) {
-        return console.log('rock')
+        return 'rock';
     }
     else if (random == 1) {
-        return console.log('paper')
+        return 'paper';
     }
 
-    else return console.log('scissors')
+    else return 'scissors'
 
 }
 let humanScore = 0;
 let computerScore = 0;
 
 function getHumanChoice() {
-    let prompt = window.prompt();
-    return prompt;
+
+    let prompt = window.prompt('Rock, Paper, or Scissors?');
+    return prompt.toLowerCase();
 }
-getHumanChoice();
-getComputerChoice();
-console.log(Math.floor(Math.random()*3));
+
+function playRound(getHumanChoice, getComputerChoice) {
+    if (getComputerChoice == 'rock' && getHumanChoice == 'paper'){
+        humanScore++
+        return 'You win! Paper beats Rock'
+    }
+    else if (getComputerChoice == 'paper' && getHumanChoice == 'scissors'){
+        humanScore++
+        return 'You win! Scissors beats Paper'
+    }
+    else if (getComputerChoice == 'scissors' && getHumanChoice == 'rock'){
+        humanScore++
+        return 'You win! Rock beats Scissors'
+    }
+    else if (getComputerChoice == getHumanChoice){
+        return 'It\'s a tie!'
+    }
+    else {
+        computerScore++
+        return 'You lose! ' + getComputerChoice.charAt(0).toUpperCase() + getComputerChoice.slice(1) + ' beats ' + getHumanChoice
+    }
+}
+
+    function playGame() {
+        for (let i = 0; i < 5; i++) {
+            console.log(playRound(getHumanChoice(), getComputerChoice()));
+        }
+        if (humanScore > computerScore) {
+            return 'You are the overall winner! ' + humanScore + ' to ' + computerScore;
+        }
+        else if (computerScore > humanScore) {
+            return 'The computer is the overall winner! ' + computerScore + ' to ' + humanScore;
+        }
+        else {
+            return 'The game is a tie! ' + humanScore + ' to ' + computerScore;
+        }
+    }
+
+
+console.log(playGame());
